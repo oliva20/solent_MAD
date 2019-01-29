ab0023d0a9a594ebd7cc6fa8768ee022b13a005a
3oliva41 <3oliva41@solent.ac.uk>
2019-01-29 15:45:24 +0000

    updated

---
+++ AAA_MyClassWork/session2-master/app/src/main/java/uk/ac/solent/mapping/SetLocationActivity.java
@@ -0,0 +1,40 @@
+package uk.ac.solent.mapping;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+
+public class SetLocationActivity extends AppCompatActivity implements View.OnClickListener {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setLocation);
+        Button regular = (Button) findViewById(R.id.btn);
+        regular.setOnClickListener(this);
+    }
+
+    public void onClick(View v)
+    {
+        Intent intent = new Intent();
+        Bundle bundle=new Bundle();
+        boolean setlocation=true;
+
+        EditText etLat = (EditText)findViewById(R.id.lat);
+        EditText etLon = (EditText)findViewById(R.id.lon);
+
+        String stringEtLat = etLat.getText().toString();
+        String stringEtLon = etLon.getText().toString();
+
+
+        bundle.putBoolean("com.example.setlocation",setlocation);
+        bundle.putString("stringLatitude", stringEtLat);
+        bundle.putString("stringLongitude", stringEtLon);
+        intent.putExtras(bundle);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+}
